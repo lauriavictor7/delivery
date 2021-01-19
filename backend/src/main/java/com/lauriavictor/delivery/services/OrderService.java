82c11234f521b82e816ea5aec19f8ba7ffc4b804
Natalia Castanha <victor7lauria@live.com>
2021-01-19 00:08:13 -0300

    GET /orders

---
+++ backend/src/main/java/com/lauriavictor/delivery/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.lauriavictor.delivery.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lauriavictor.delivery.dto.OrderDTO;
+import com.lauriavictor.delivery.entities.Order;
+import com.lauriavictor.delivery.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
